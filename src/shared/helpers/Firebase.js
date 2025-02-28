58ee4dc07fcbbd2590b814db4cdad3cb172a7f47
Reymart Bordo <62002257+Mart-007@users.noreply.github.com>
2025-02-28 21:49:40 +0800

    Update Firebase.js helpers

--- src/shared/helpers/Firebase.js
+++ src/shared/helpers/Firebase.js
@@ -5,13 +5,7 @@ import { getDatabase } from 'firebase/database';
 // import { API_KEY } from '../config/environments'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA793IQCkTcixZktaNh5pUAGewce7jfdh4',
-  authDomain: 'restaurant-utk.firebaseapp.com',
-  projectId: 'restaurant-utk',
-  storageBucket: 'restaurant-utk.appspot.com',
-  messagingSenderId: '1032973753701',
-  appId: '1:1032973753701:web:9c8f84e4300efae984d58c',
-  measurementId: 'G-FTQ3L5RSN4',
+ 
 };
 
 const app = initializeApp(firebaseConfig);
