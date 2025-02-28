5643bebf0dd0b9443cce6f715cc3aa4384b54b38
Reymart Bordo <reymart@3enix.com>
2025-02-28 22:19:24 +0800

    fix env

--- src/shared/config/environments.js
+++ src/shared/config/environments.js
@@ -1,10 +1,12 @@
-export const {
-  API_KEY,
-  AUTH_DOMAIN,
-  PROJECT_ID,
-  STORAGE_BUCKET,
-  MESSAGING_SENDING_ID,
-  APP_ID,
-  MEAUREMENT_ID,
+const ENV = {
+  API_KEY: process.env.API_KEY,
+  AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+  PROJECT_ID: process.env.PROJECT_ID,
+  STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+  MESSAGING_SENDING_ID: process.env.MESSAGING_SENDING_ID,
+  APP_ID: process.env.APP_ID,
+  MEAUREMENT_ID: process.env.MEAUREMENT_ID,
   // eslint-disable-next-line no-undef
-} = process.env;
+};
+
+export default ENV;
--- src/shared/helpers/Firebase.js
+++ src/shared/helpers/Firebase.js
@@ -2,10 +2,16 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getDatabase } from 'firebase/database';
-// import { API_KEY } from '../config/environments'
+import CONFIG from '../config/environments';
 
 const firebaseConfig = {
- 
+  apiKey: CONFIG.API_KEY,
+  authDomain: CONFIG.AUTH_DOMAIN,
+  projectId: CONFIG.PROJECT_ID,
+  storageBucket: CONFIG.STORAGE_BUCKET,
+  messagingSenderId: CONFIG.MESSAGING_SENDING_ID,
+  appId: CONFIG.APP_ID,
+  measurementId: CONFIG.MEAUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
